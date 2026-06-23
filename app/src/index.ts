597aa507162e1415d164a8d3682f0499df4f1d02
tong lei <2245163998@qq.com>
2026-06-23 12:41:46 +0800

    chore: remove official account modules

--- app/src/index.ts
+++ app/src/index.ts
@@ -3,7 +3,6 @@ import {Menus} from "./menus";
 import {Model} from "./layout/Model";
 import {onGetConfig} from "./boot/onGetConfig";
 import {initBlockPopover} from "./block/popover";
-import {account} from "./config/account";
 import {addScript, addScriptSync} from "./protyle/util/addScript";
 import {genUUID} from "./util/genID";
 import {fetchGet, fetchPost} from "./util/fetch";
@@ -228,7 +227,6 @@ export class App {
                     fetchPost("/api/setting/getCloudUser", {}, userResponse => {
                         window.siyuan.user = userResponse.data;
                         onGetConfig(response.data.start, this);
-                        account.onSetaccount();
                         setTitle("", true);
                         initMessage();
                         /// #if BROWSER && !MOBILE
